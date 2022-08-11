380b263c64221ac5080765148e2292bc848f3cd8
Isaac Fierro <zifg_36@live.com.mx>
2022-08-11 09:06:31 -0700

    Cambios a produccion

--- core/settings/production.py
+++ core/settings/production.py
@@ -8,10 +8,10 @@ ALLOWED_HOSTS = []
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd8a6i3oarqp0ki',
-        'USER': 'zmtrjsodzyeijv',
-        'PASSWORD': '2b998cd5b434683e9d981d43087c18eb80ca197adfc120deb1732d07d59b5088',
-        'HOST': 'ec2-34-233-115-14.compute-1.amazonaws.com',
+        'NAME': 'd4gr7q38j7el0o',
+        'USER': 'ilagjelqhzcodz',
+        'PASSWORD': '453819f31319cbf4a6739cf9b496af818dd05b8e3a23b5bf5db34f50442de442',
+        'HOST': 'ec2-34-203-182-65.compute-1.amazonaws.com',
         'PORT': 5432
     }
 }
