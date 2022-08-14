871c4cf09e99bad133565cb1f1ea978dcd4266a6
Isaac Fierro <isaaczid@hotmail.com>
2022-08-14 15:50:58 -0700

    Credenciales en la base de datos

--- core/settings/production.py
+++ core/settings/production.py
@@ -8,11 +8,11 @@ ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd2d8q0mpatkf2s',
-        'USER': 'xfodvptcljrojx',
-        'PASSWORD': '8a3c5c80d0e8b37ca3e93f49b0be005aa9b92d63d8359078e2b6a631e1790e78',
-        'HOST': 'ec2-50-19-255-190.compute-1.amazonaws.com',
-        'PORT': 5432
+        'NAME': 'dqthvvpvlih04',
+        'USER': 'pkcmvxueqsgnfq',
+        'PASSWORD': '238bde66551b0fe4939ae87eddaedf077b428432f4495de5973a551a441e46d4',
+        'HOST': 'ec2-44-195-100-240.compute-1.amazonaws.com',
+        'PORT': 5432,
     }
 }
 
