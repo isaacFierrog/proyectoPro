178fba461c899662a1db517be6888fe51118222c
Isaac Fierro <isaaczid@hotmail.com>
2022-08-14 16:21:30 -0700

    Cambios al archivo base

--- core/settings/base.py
+++ core/settings/base.py
@@ -4,9 +4,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-print('*' * 60)
-print(BASE_DIR)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
