4d9e80d44d0465a51f376e87f2c14033381309cb
Isaac Fierro <zifg_36@live.com.mx>
2022-08-16 09:20:00 -0700

    Todos los origenes permitidos

--- core/settings/base.py
+++ core/settings/base.py
@@ -107,13 +107,17 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-CORS_ALLOWED_ORIGINS = [
-    "*",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
-CORS_ORIGIN_WHITELIST = [
-    "*"
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:8080",
+#     "http://127.0.0.1:5500/",
+# ]
+
+# CORS_ORIGIN_WHITELIST = [
+#     "http://localhost:8080",
+#     "http://127.0.0.1:5500/",
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
