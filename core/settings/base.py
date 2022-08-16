7099e53833f02ad29e5cadeb2f89fc46b54adc99
Isaac Fierro <zifg_36@live.com.mx>
2022-08-16 08:55:22 -0700

    Cambios en CORS

--- core/settings/base.py
+++ core/settings/base.py
@@ -112,6 +112,11 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500/",
 ]
 
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8080",
+    "http://127.0.0.1:5500/",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
